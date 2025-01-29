4e35f375b86aa96eeb5d60c7b54efa5edd15d365
Sachin Kumar Yadav <sachinkyadav457@gmail.com>
2025-01-29 19:22:12 +0530

    New Programs

---
+++ TwoSum.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 14;
+
+        Solution sol = new Solution();
+        int[] result = sol.twoSum(nums, target);
+
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[] {};
+
+    }
+}
